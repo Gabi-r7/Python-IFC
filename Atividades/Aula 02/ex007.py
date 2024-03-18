3c202ad430ddbb2236ff23c4f2d0f95889c2af20
Gabriel <jairolamb.pzo@gmail.com>
2024-03-18 15:10:36 -0300

    atividades

---
+++ Atividades/Aula 02/ex007.py
@@ -0,0 +1,4 @@
+valorPago = float(input('Digite o valor que deseja abastecer: '))
+valorLitro = float(input('Digite o valor do litro de gasolina: '))
+valorAbastecido = valorPago / valorLitro
+print(f'Você abasteceu {valorAbastecido:.2f}L.')
---
+++ Atividades/Aula 02/ex008.py
@@ -0,0 +1,5 @@
+tara = float(input('Digite o peso da Tara: '))
+prato = float(input('Digite o peso do Prato: '))
+prato-= tara
+valorPagar = prato * 45
+print(f'O valor a pagar é de R${valorPagar:.2f}!')
