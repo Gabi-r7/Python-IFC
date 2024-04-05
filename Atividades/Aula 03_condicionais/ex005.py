627b055f127848a85ea47b0e14a2cbd60402ee96
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2024-04-05 14:43:24 +0000

    arrumando bugs

--- Atividades/Aula 03_condicionais/ex005.py
+++ Atividades/Aula 03_condicionais/ex005.py
@@ -3,16 +3,16 @@ tipo = input('Digite o tipo de combustível (A = Álcool|G = Gasolina): ')
 
 if tipo == 'A':
     if litros < 20:
-        total = (6.50 * litros) - (litros * 0.03)
+        total = ((6.50 * 0.97) * litros)
     elif litros >= 20:
-        total = (6.50 * litros) - (litros * 0.05)
+        total = ((6.50 * 0.95)* litros)
     else:
         print('Erro! Quantidade inválida!')
 elif tipo == 'G':
     if litros < 20:
-        total = (7.20 * litros) - (litros * 0.04)
+        total = ((7.20 * 0.96)* litros)
     elif litros >= 20:
-        total = (7.20 * litros) - (litros * 0.06)
+        total = ((7.20 * 0.94)* litros)
     else:
         print('Erro! Quantidade inválida!')
 else:
--- Atividades/Aula 03_condicionais/ex006.py
+++ Atividades/Aula 03_condicionais/ex006.py
@@ -3,18 +3,17 @@ mes = int(input('Digite os meses: '))
 ano = int(input('Digite os anos: '))
 diasS = int(input('Digite quantos dias quer somar: '))
 
+diasS += dia
+dia = 0
 if diasS > 365:
-    anosAMais = round(diasS / 365)  #adiciona aos anos a quantidade a mais e arredonda
+    anosAMais = diasS // 365  #adiciona aos anos a quantidade a mais e arredonda
     ano += anosAMais
     diasS -= anosAMais * 365
-elif diasS > 30:
-    mesesAMais = round(diasS / 30)
+if diasS > 30:
+    mesesAMais = diasS // 30
     mes += mesesAMais
     diasS -= mesesAMais * 30
-elif diasS > 0:
+if diasS > 0:
     dia += diasS
-    # if dia > 30:
-    #     mes += 1
-    #     dia
-
+    
 print(f'dia: {dia}\n mes: {mes}\n ano: {ano}')
