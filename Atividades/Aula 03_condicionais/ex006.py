6bd6bbcb483b11b580a67269024ba31388488a21
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2024-04-05 08:36:29 -0300

    arrumando condicionais

--- Atividades/Aula 03_condicionais/ex006.py
+++ Atividades/Aula 03_condicionais/ex006.py
@@ -3,34 +3,18 @@ mes = int(input('Digite os meses: '))
 ano = int(input('Digite os anos: '))
 diasS = int(input('Digite quantos dias quer somar: '))
 
-
-anosAMais = round(diasS / 365)  #adiciona aos anos a quantidade a mais e arredonda
-ano += anosAMais
-diasS -= anosAMais * 365
-mesesAMais = round(diasS / 30)
-mes += mesesAMais
-diasS -= mesesAMais * 30
-dia += diasS
+if diasS > 365:
+    anosAMais = round(diasS / 365)  #adiciona aos anos a quantidade a mais e arredonda
+    ano += anosAMais
+    diasS -= anosAMais * 365
+elif diasS > 30:
+    mesesAMais = round(diasS / 30)
+    mes += mesesAMais
+    diasS -= mesesAMais * 30
+elif diasS > 0:
+    dia += diasS
+    # if dia > 30:
+    #     mes += 1
+    #     dia
 
 print(f'dia: {dia}\n mes: {mes}\n ano: {ano}')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# dia += diasS
-# if dia > 30: 
-#     aux = dia #auxiliar recebe dia
-#     aux -= 30 #auxiliar tira o valor de um mes e fica com a diferença
-#     dia -= aux #a diferença é retirada do dia, dia = 30
-#     mes += (aux / 30)
