0f0e7bb9a15b6ae74b05a18935c1a8be10cd8d22
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2024-12-11 16:06:41 +0000

    exercícios

---
+++ Programacao_1/Lista_ex_listas/ex021.py
@@ -0,0 +1,49 @@
+# 21. Faça um programa que carregue uma lista com os modelos de cinco carros (exemplo de modelos: FUSCA, GOL, VECTRA etc). Carregue uma outra lista com o consumo desses carros, isto é, quantos quilômetros cada um desses carros faz com um litro de combustível. Calcule e mostre: 
+# a. O modelo do carro mais econômico; 
+# b. Quantos litros de combustível cada um dos carros cadastrados consome para percorrer uma distância de 1000 quilômetros e quanto isto custará, considerando um que a gasolina custe R$ 2,25 o litro. Abaixo segue uma tela de exemplo. O disposição das informações deve ser o mais próxima possível ao exemplo. Os dados são fictícios e podem mudar a cada execução do programa. 
+# Comparativo de Consumo de Combustível 
+# Veículo 1 
+# Nome: fusca 
+# Km por litro: 7 
+# Veículo 2 
+# Nome: gol 
+# Km por litro: 10 
+# Veículo 3 
+# Nome: uno 
+# Km por litro: 12.5 
+# Veículo 4 
+# Nome: Vectra
+# Km por litro: 9 
+# Veículo 5 
+# Nome: Peugeout 
+# Km por litro: 14.5 
+# Relatório Final 
+#  1 - fusca - 7.0 - 142.9 litros - R$ 321.43 
+#  2 - gol - 10.0 - 100.0 litros - R$ 225.00 
+#  3 - uno - 12.5 - 80.0 litros - R$ 180.00 
+#  4 - vectra - 9.0 - 111.1 litros - R$ 250.00 
+#  5 - peugeout - 14.5 - 69.0 litros - R$ 155.17 
+# O menor consumo é do peugeout. 
+
+carros = []
+consumo_carros = []
+consumo_exemplo = []
+gasto_exemplo = []
+
+for i in range(5):
+    carros.append(input('Digite o nome de um carro: '))
+
+for i in range(5):
+    consumo_carros.append(float(input(f'Digite o consumo do {carros[i]}: ')))
+
+for i in range(5):
+    consumo_exemplo.append(1000 / consumo_carros[i])
+    gasto_exemplo.append(consumo_exemplo[i] * 2.25)
+
+print('Comparativos de combustível: ')
+for i in range(5):
+    print(f'Veículo {i}\nNome: {carros[i]}\nKm por litro: {consumo_carros[i]}')
+
+print('Relatório Final: ')
+for i in range(5):
+    print(f'{i+1} - {carros[i]} - {consumo_carros[i]} - {consumo_exemplo[i]:.2f} litros - R$ {gasto_exemplo[i]:.2f}')
